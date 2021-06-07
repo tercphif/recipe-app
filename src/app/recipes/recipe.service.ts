a59f312e97deea7be57be9bfff8b208463c6b7bc
Terrance Phifer <tercphif@gmail.com>
2021-06-07 17:29:28 -0500

    Save-Fetch
    Added Save/Fetch features using Firebase

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -9,29 +9,35 @@ import { ShoppingListService } from '../shopping-list/shopping-list.service';
 export class RecipeService {
   recipesChanged = new Subject<Recipe[]>();
 
-  private recipes: Recipe[] = [
-    new Recipe(
-      'Pan Seared Steak',
-      'A medium rare sirloin with garlic butter and herbs',
-      'https://cookingpotsnpans.com/wp-content/uploads/2020/09/london-broil.jpg',
-      [
-        new Ingredient('Steak', 1),
-        new Ingredient('Butter', 1),
-        new Ingredient('Garlic', 2),
-        new Ingredient('Rosemary', 6)
-      ]),
-    new Recipe(
-      'Pizza Casserole',
-      'Pan made and extra cheesy - This casserole is delicious',
-      'https://joyfoodsunshine.com/wp-content/uploads/2016/09/easy-pizza-casserole-recipe-4-500x500.jpg',
-      [
-        new Ingredient('Cheese', 3),
-        new Ingredient('Pepperoni', 20),
-        new Ingredient('Pizza Dough', 1)
-      ])
-  ];
+  //private recipes: Recipe[] = [
+  //  new Recipe(
+  //    'Pan Seared Steak',
+  //    'A medium rare sirloin with garlic butter and herbs',
+  //    'https://cookingpotsnpans.com/wp-content/uploads/2020/09/london-broil.jpg',
+  //    [
+  //      new Ingredient('Steak', 1),
+  //      new Ingredient('Butter', 1),
+  //      new Ingredient('Garlic', 2),
+  //      new Ingredient('Rosemary', 6)
+  //    ]),
+  //  new Recipe(
+  //    'Pizza Casserole',
+  //    'Pan made and extra cheesy - This casserole is delicious',
+  //    'https://joyfoodsunshine.com/wp-content/uploads/2016/09/easy-pizza-casserole-recipe-4-500x500.jpg',
+  //    [
+  //      new Ingredient('Cheese', 3),
+  //      new Ingredient('Pepperoni', 20),
+  //      new Ingredient('Pizza Dough', 1)
+  //    ])
+  //];
+  private recipes: Recipe[] = [];
 
   constructor(private slService: ShoppingListService) {}
+
+  setRecipes(recipes: Recipe[]) {
+    this.recipes = recipes;
+    this.recipesChanged.next(this.recipes.slice());
+  }
 
   getRecipes() {
     return this.recipes.slice();
